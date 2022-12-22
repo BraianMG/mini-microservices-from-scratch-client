d23ce6065dea8ad21717259126f4ea17398ee04d
Braian Gonzales <braian.gonzales@wakapi.com>
2022-12-22 10:57:07 -0300

    create comments

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
 
 const PostList = () => {
   const [posts, setposts] = useState({});
@@ -19,8 +20,9 @@ const PostList = () => {
           key={post.id}
         >
           <div className='card-body'>
-            <h3>{post.title}</h3>  
-          </div>          
+            <h3>{post.title}</h3>
+            <CommentCreate postId={post.id}/>
+          </div>
         </div>
       )
     }
